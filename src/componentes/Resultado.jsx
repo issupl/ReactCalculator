7e32e49e6d5063b2b82691d8640c217d69c1ca79
Israel <issupl@protonmail.com>
2022-03-23 09:37:40 +0100

    AHORA FUNCIONA - Quito comentarios y la variable salida como variable para que se modifique el estado acualizándola por props.operaciones

--- src/componentes/Resultado.jsx
+++ src/componentes/Resultado.jsx
@@ -6,7 +6,7 @@ const [salida,setSalida] = useState(0);
     let oper1 = parseInt(dato.primerOper,10);
     let oper2 = parseInt(dato.segundoOper,10);
     let op;
-    console.log('dato: ',oper1 ,oper2,oper1*oper2,props.operaciones)
+    
 
     useEffect(()=>{
         
@@ -14,22 +14,26 @@ const [salida,setSalida] = useState(0);
     
             case "*" :
              op= oper1 * oper2;
+             
             setSalida(op);
             break;
             case "+" : 
             op = oper1 + oper2;
+            console.log("op:",op)
             setSalida(op);
              break;
             case "-" : 
             op = oper1 - oper2;
+            
             setSalida(op); break;
             case "/" : 
             op= oper1 / oper2;
+            
             setSalida(op);
              break;
-            default: setSalida("");
+            default:  setSalida("");break;
         }
-    },[salida])
+    },[props.operaciones])
 
     return(<div>{salida}</div>)
 }
