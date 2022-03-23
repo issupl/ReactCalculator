6c5e2c00a373dc82d73f9cb7c641d4504b6020dd
Israel <issupl@protonmail.com>
2022-03-23 12:00:47 +0100

    Agrego una función para hacer la operación

--- src/helpers/misFunciones.js
+++ src/helpers/misFunciones.js
@@ -9,3 +9,18 @@ const dato = {
 export default function elDato(){
     return dato;
 }
+
+export function selecOperacion(operacion,dato){
+    let op;
+    let oper1 = parseInt(dato.primerOper,10);
+    let oper2 = parseInt(dato.segundoOper,10);
+    switch(operacion){
+        case "*" :op= oper1 * oper2;break;
+        case "+" :op = oper1 + oper2;break;
+        case "-" :op = oper1 - oper2;break;
+        case "/" :op= oper1 / oper2;break; 
+        default:op="";break;
+    }
+
+    return op;
+}
