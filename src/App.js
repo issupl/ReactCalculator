6679e88af36a946054509f2b5f277fe6ea58f8db
Israel <issupl@protonmail.com>
2022-03-23 12:03:31 +0100

    Agrego un useEffect para utilizar la función agregada

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import './App.css';
 import Boton from './componentes/Boton';
 import Input from './componentes/Input';
 import Resultado from './componentes/Resultado';
-import elDato from './helpers/misFunciones.js';
+import elDato,{selecOperacion} from './helpers/misFunciones.js';
 function App() {
   
   const [datos,setDatos] = useState(elDato);
   const [operacion,setOperacion] = useState("");
+  const [salidFun,setSalidaFun] = useState(0);
+
 
   function changeFirstNumberHandler (event) {
     event.target.name === "primerOper" ? setDatos({...datos,segundoOper:event.target.value}) : setDatos({...datos,primerOper:event.target.value});  
   }
 
+  useEffect(()=>{
+    const laFuncion = ()=>{     
+      setSalidaFun(selecOperacion(operacion,datos));
+    }
+    laFuncion();
+  },[operacion])
   
 
   return (
@@ -22,6 +30,7 @@ function App() {
       <Input unaFuncion={changeFirstNumberHandler} />
       <Boton lasOperaciones={datos.operacionEscogida} setOper={setOperacion}/>
       <Resultado datosOperando = {datos} operaciones={operacion}/>
+      <div>Salida de la función: {salidFun}</div>
     </div>
   );
 }
