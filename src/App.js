505d4f7e4c48322f46500fdeb13294f27c347166
Israel <issupl@protonmail.com>
2022-03-23 09:33:31 +0100

    Quito comentarios y useEffect

--- src/App.js
+++ src/App.js
@@ -1,37 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Boton from './componentes/Boton';
 import Input from './componentes/Input';
 import Resultado from './componentes/Resultado';
 import elDato from './helpers/misFunciones.js';
 function App() {
-  const [ firstNumber, setFirstNumber ] = useState("");
+  
   const [datos,setDatos] = useState(elDato);
   const [operacion,setOperacion] = useState("");
+
   function changeFirstNumberHandler (event) {
-    console.log('el evento: ',event.target.value,event.target.name)
-    if(event.target.name === "primerOper"){
-      setDatos({...datos,segundoOper:event.target.value})
-    }else{
-
-      setDatos({...datos,primerOper:event.target.value})
-    }
-
-    /*setFirstNumber(event.target.value);*/
+    event.target.name === "primerOper" ? setDatos({...datos,segundoOper:event.target.value}) : setDatos({...datos,primerOper:event.target.value});  
   }
 
-  useEffect(()=>{
-
-    const primer=()=>{
-      
-      /*setDatos({...datos,primerOper:44})*/
-      console.log('operacion: ',operacion)
-      console.log('primerOper: ',datos.primerOper);
-      console.log('segundoOper: ',datos.segundoOper);
-    }
-    primer();
-  }
-  ,[operacion]);
+  
 
   return (
     <div>
