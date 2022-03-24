ddc6dd5cb9b808315a7d6dcc3bad69cb8c973fae
Israel <issupl@protonmail.com>
2022-03-24 12:28:26 +0100

    Modificando para el boton C en el evento

--- src/App.js
+++ src/App.js
@@ -9,11 +9,20 @@ function App() {
   const [datos,setDatos] = useState(elDato);
   const [operacion,setOperacion] = useState("");
   const [salidFun,setSalidaFun] = useState(0);
+  const [paraElC,setParaElC] = useState(0);
 
-
-  function changeFirstNumberHandler (event) {
-    event.target.name === "primerOper" ? setDatos({...datos,segundoOper:event.target.value}) : setDatos({...datos,primerOper:event.target.value});  
+  function changeFirstNumberHandler (event,paraElC) {
+    console.log('En changeFirstNumberHandler paraElC:',paraElC)
+    if(paraElC === "1" || paraElC === 1){
+      console.log('paraElC ..1:',paraElC)
+      event.target.name === "primerOper" ? setDatos({...datos,segundoOper:event.target.value}) : setDatos({...datos,primerOper:event.target.value});  
+    }else{
+      event.target.value = 0;  
+      setParaElC(1);
+      console.log('paraElC ..0: ',paraElC)
+    }
   }
+  
 
   useEffect(()=>{
     const laFuncion = ()=>{     
@@ -27,9 +36,9 @@ function App() {
     <div>
       <h1>Calculadora</h1>
      
-      <Input unaFuncion={changeFirstNumberHandler} />
+      <Input unaFuncion={changeFirstNumberHandler} lasOperacion={operacion} paraElC={paraElC} setParaElC={setParaElC}/>
       <Boton lasOperaciones={datos.operacionEscogida} setOper={setOperacion}/>
-      <Resultado datosOperando = {datos} operaciones={operacion}/>
+      <Resultado datosOperando = {datos} operaciones={operacion} paraElC={paraElC} setDatos={setDatos} setParaElC={setParaElC} setOperacion={setOperacion}/>
       <div>Salida de la función: {salidFun}</div>
     </div>
   );
