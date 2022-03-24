605ef68915272278337f11cfd88f293215991e2a
Israel <issupl@protonmail.com>
2022-03-24 12:27:17 +0100

    Agregando para resetear con el botón C

--- src/componentes/Input.jsx
+++ src/componentes/Input.jsx
@@ -1,11 +1,20 @@
-import React, {useState} from "react";
+import React, {useState,useEffect} from "react";
 
 function Input(entradas){
-    
- const valoresNombreInput = ["primerOper", "segundoOper"];
-
+    const [elC,setElC] = useState(0);
+    const valoresNombreInput = ["primerOper", "segundoOper"];
+ useEffect(()=>{
+     setElC(entradas.paraElC) ;
+   
+     console.log('entradas en Input: ', entradas)
+     console.log(`el elC ${elC}`);
+     for(let elemento of document.querySelectorAll('input')){
+         elemento.value = 0;
+     }
+     
+ },[entradas.paraElC])
  let salida = valoresNombreInput.map((item,index)=>{
-     return <input type="text" name={item} key={index} onChange={(event)=>{entradas.unaFuncion(event)}}/>
+     return <input type="text" name={item} key={index} onChange={(event)=>{entradas.unaFuncion(event,elC)}}/>
  })
     return(<>Los input:   {salida}</>);
 
